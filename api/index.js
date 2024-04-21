41a8104eb4086e8ce30805df2e4b6be0427f0ba7
Nikillan Rajesh <nikillanrajesh@gmail.com>
2024-04-21 13:42:00 +0530

    posting works!!

--- api/index.js
+++ api/index.js
@@ -1,47 +1,51 @@
 import express from "express";
 const app = express();
-import userRoutes from "./routes/users.js"
-import postRoutes from "./routes/posts.js"
-import likeRoutes from "./routes/likes.js"
-import commentRoutes from "./routes/comments.js"
-import authRoutes from "./routes/auth.js"
-import cors from "cors"
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import likeRoutes from "./routes/likes.js";
+import commentRoutes from "./routes/comments.js";
+import authRoutes from "./routes/auth.js";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import multer from "multer";
 //middlewares
-app.use((req,res,next)=>{
-    res.header("Access-Control-Allow-Credentials",true)
-    next()
-})
-app.use(express.json())
-app.use(cors({
-    origin: "http://localhost:3000"
-}))
-app.use(cookieParser())
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+app.use(cookieParser());
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, '../client/public/upload')
-    },
-    filename: function (req, file, cb) {
-      cb(null, Date.now()+file.originalname)
-    }
-  })
-  
-  const upload = multer({ storage: storage })
-
-app.post("/api/upload", upload.single("file"),(res,req)=>{
-    const file = req.file;
-    res.statusCode(200).json(file.filename)
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    let filename = Date.now().toString() + file.originalname;
+    file.filename = filename;
+    cb(null, filename);
+  },
 });
 
-app.use("/api/users",userRoutes);
-app.use("/api/posts", postRoutes); 
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  console.dir(file);
+  return res.status(200).json(file.filename);
+});
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 app.use("/api/likes", likeRoutes);
-app.use("/api/comments", commentRoutes); 
+app.use("/api/comments", commentRoutes);
 app.use("/api/auth", authRoutes);
 
-app.listen(8800,()=>{
-    console.log("API Working")
-})
-
+app.listen(8800, () => {
+  console.log("API Working");
+});
